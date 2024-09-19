ec1f9e115be5c4eb9829d9ba0ec43dd53c478cfc
Abdul Habra <abdul.habra@gmail.com>
2024-09-18 22:17:25 -0400

    optimize ClassicReflect to make it faster

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ Discover performance impact of different Java libraries.
 Three approaches are tested:
 
 1. Using `org.apache.commons.lang3.reflect`  FieldUtils and MethodUtils. This is the slowest approach.
-2. Using classic Java reflection `Class` getField and getMethod. About twice as fast as using apache.
-3. Using the `java.lang.invoke` VarHandle and MethodHandle (added in Java 1.7). About three times faster than classic.
+2. Using classic Java reflection `Class` getField and getMethod. About 4 to 5 times as fast as using apache.
+3. Using the `java.lang.invoke` VarHandle and MethodHandle (added in Java 1.7). Similar to classic.
+
+Observe that when using classic reflection or invoke package, the major performance gain is achieved because we 
+can get a reference to the method or the field, cach it , then reuse it during the repeated tests.
 
--- src/main/java/com/tek271/javaperf/reflection/ClassicReflect.java
+++ src/main/java/com/tek271/javaperf/reflection/ClassicReflect.java
@@ -2,17 +2,39 @@ package com.tek271.javaperf.reflection;
 
 import com.google.common.base.Verify;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import static com.tek271.javaperf.reflection.Book.HITCHHIKER;
 
 public class ClassicReflect {
+	private static final Field TITLE_FIELD = getField(Book.class, "title");
+	private static final Field PAGES_FIELD = getField(Book.class, "pages");
+	private static final Method CALC_VALUE_METHOD = getMethod(Book.class, "calcValue");
+
+	private static Field getField(Class<?> cls, String fieldName) {
+		try {
+			return cls.getField(fieldName);
+		} catch (NoSuchFieldException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static Method getMethod(Class<?> cls, String methodName) {
+		try {
+			return cls.getMethod(methodName);
+		} catch (NoSuchMethodException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	public void readFields() {
 		String title;
 		int pages;
 
 		try {
-			title = (String) Book.class.getField("title").get(HITCHHIKER);
-			pages = Book.class.getField("pages").getInt(HITCHHIKER);
+			title = (String) TITLE_FIELD.get(HITCHHIKER);
+			pages = PAGES_FIELD.getInt(HITCHHIKER);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
@@ -23,7 +45,7 @@ public class ClassicReflect {
 	public void callMethod() {
 		long value;
 		try {
-			value = (Long) Book.class.getMethod("calcValue").invoke(HITCHHIKER);
+			value = (Long) CALC_VALUE_METHOD.invoke(HITCHHIKER);
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
